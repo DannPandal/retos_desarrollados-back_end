984517ba6f6b18683bdb23978fa4662d8358958a
DannPandal <dev.pandaldn@gmail.com>
2022-04-11 14:57:19 -0500

    reto 5

---
+++ Semana02/Semana02-Reto5/README.md
@@ -0,0 +1,17 @@
+# Seguir los siguientes pasos para la ejecucion
+
+##  1. Crear un un ambiente virtual\
+
+##  2. Intalar las los requerimients.txt
+
+## 3. Ejecutar app.py
+    $> python -m flask run
+    $> flask run
+    $> flask run -p 8080
+
+## GET - Retornar lista json con todos los datos
+![Image text](https://github.com/DannPandal/retos_desarrollados-back_end/blob/main/Semana02/Semana02-Reto5/assets/all_data.png)
+
+## POST - Agregar elemento Json al diccionario contenedor
+![Image text](https://github.com/DannPandal/retos_desarrollados-back_end/blob/main/Semana02/Semana02-Reto5/assets/add_data.png)
+
--- Semana02/Semana02-Reto5/app.py
+++ Semana02/Semana02-Reto5/app.py
@@ -2,15 +2,44 @@
 # ! Reto 5: Desarrollar una API para ingresar datos y mostrarlos
 # * Desarrollar una API para ingresar los datos (libre) y que por medio de otro endpoint los devuelva.
 
-from flask import Flask, jsonify
+from flask import Flask,request, jsonify
+import uuid
 
 app = Flask(__name__)
+
+data = {}
+
+RESPONSE_ERROR = "Error al procesar la petición"
+RESPONSE_ERROR_KEY = "No se encontro el producto con el id ingresado"
 
 @app.route('/')
 def welcome():
     return "Bienvenido a la API en Flask datos"
 
+# * GET - devolver todo el contenido
+@app.route('/all_data', methods=['GET'])
+def all_data():
+    try:
+        #! jsonify indica el formato de salida application/json
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"message": RESPONSE_ERROR, "error": str(e)})
 
+# * POST - agregar datos
+@app.route('/add_data', methods=['POST'])
+def add_data():
+    try:
+        # * Obtener datos enviados
+        data_json = request.get_json()
+        data[str(uuid.uuid4())] = data_json
+        #! jsonify indica el formato de salida application/json
+        return jsonify({"message": "data agregada correctamente"})
+    except Exception as e:
+        return jsonify({"message": RESPONSE_ERROR, "error": str(e)})
+
+
+if __name__ == "__main__":
+    app.run()
 
 
 
